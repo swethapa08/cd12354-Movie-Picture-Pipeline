cedb3efbd23435c065b91d1e1d74dba4061bc7e2
swethapa08 <karthi@Karthis-MacBook-Pro.local>
2026-09-05 00:09:44 +0530

    CI CD Workflow

--- starter/frontend/src/components/MovieList.js
+++ starter/frontend/src/components/MovieList.js
@@ -6,7 +6,9 @@ function MovieList({ onMovieClick }) {
   const [movies, setMovies] = useState([]);
 
   useEffect(() => {
-    axios.get(`${process.env.REACT_APP_MOVIE_API_URL}/movies`).then((response) => {
+    const movieApiUrl = process.env.REACT_APP_MOVIE_API_URL || '';
+
+    axios.get(`${movieApiUrl}/movies`).then((response) => {
       setMovies(response.data.movies);
     });
   }, []);
